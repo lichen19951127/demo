8f575d789753c4420fb5f3364cceb2126431eaa2
lichen <594281739@qq.com>
2019-06-26 11:17:58 +0800

    111111111

---
+++ JavaSccriptDemo/JavaSccriptDemo.0626/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JavaSccriptDemo._0626.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
